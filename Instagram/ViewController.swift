65999ad3fedf19bd7856ecf2e96b845ae6e6011a
Wesley Osborne <wesleyosborne1@gmail.com>
2021-02-14 13:38:45 -0500

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Wesley Osborne on 2/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
