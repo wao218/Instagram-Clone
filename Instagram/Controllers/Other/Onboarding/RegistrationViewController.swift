63c64014c3b0556e28ff3865d0e6e251b5b7a59a
Wesley Osborne <wesleyosborne1@gmail.com>
2021-02-15 18:21:20 -0500

    Add login screen UI elements, add fields and buttons functionality

--- Instagram/Controllers/Other/Onboarding/RegistrationViewController.swift
+++ Instagram/Controllers/Other/Onboarding/RegistrationViewController.swift
@@ -9,10 +9,10 @@ import UIKit
 
 class RegistrationViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+    view.backgroundColor = .systemBackground
+  }
 
 }
