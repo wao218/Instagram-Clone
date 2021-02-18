18217860d96fd3ad3eea35a769d832a11504a987
Wesley Osborne <wesleyosborne1@gmail.com>
2021-02-17 20:53:53 -0500

    Add more cells to Settings view controller, stub out Edit Profile view controller.

--- Instagram/Controllers/Other/General/EditProfileViewController.swift
+++ Instagram/Controllers/Other/General/EditProfileViewController.swift
@@ -9,21 +9,38 @@ import UIKit
 
 class EditProfileViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSave))
+    navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(didTapCancel))
+  }
+  
+  @objc private func didTapSave() {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  }
+  
+  @objc private func didTapCancel() {
+    
+  }
+  
+  @objc private func didTapProfilePicture() {
+    let actionSheet = UIAlertController(title: "Profile Picture", message: "Change profile picture", preferredStyle: .actionSheet)
+    
+    actionSheet.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { (_) in
+      
+    }))
+    
+    actionSheet.addAction(UIAlertAction(title: "Choose from Library", style: .default, handler: { (_) in
+      
+    }))
+    
+    actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+    
+    actionSheet.popoverPresentationController?.sourceView = view
+    actionSheet.popoverPresentationController?.sourceRect = view.bounds
+    
+    present(actionSheet, animated: true)
+  }
 
 }
--- Instagram/Controllers/Other/General/SettingsViewController.swift
+++ Instagram/Controllers/Other/General/SettingsViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SafariServices
 
 struct SettingCellModel {
   let title: String
@@ -45,12 +46,36 @@ final class SettingsViewController: UIViewController {
   }
   
   private func configureModels() {
-    let section = [
+    data.append([
+      SettingCellModel(title: "Edit Profile") { [weak self] in
+        self?.didTapEditProfile()
+      },
+      SettingCellModel(title: "Invite Friends") { [weak self] in
+        self?.didTapInviteFriends()
+      },
+      SettingCellModel(title: "Save Original Posts") { [weak self] in
+        self?.didTapSaveOriginalPosts()
+      }
+    ])
+    
+    data.append([
+        SettingCellModel(title: "Terms of Service") { [weak self] in
+          self?.openURL(type: .terms)
+        },
+        SettingCellModel(title: "Privacy Policy") { [weak self] in
+          self?.openURL(type: .privacy)
+        },
+        SettingCellModel(title: "Help / Feedback") { [weak self] in
+          self?.openURL(type: .help)
+        }
+      ])
+
+    
+    data.append([
       SettingCellModel(title: "Log Out") { [weak self] in
         self?.didTapLogOut()
       }
-    ]
-    data.append(section)
+    ])
   }
   
   // MARK: - Action Functions
@@ -83,6 +108,41 @@ final class SettingsViewController: UIViewController {
     
     present(actionSheet, animated: true)
   }
+  
+  enum SettingsURLType {
+    case terms, privacy, help
+  }
+
+  func openURL(type: SettingsURLType) {
+    let urlString: String
+    switch type {
+    case .terms: urlString = "https://help.instagram.com/1215086795543252"
+    case .privacy: urlString = "https://help.instagram.com/519522125107875"
+    case .help: urlString = "https://help.instagram.com"
+    }
+    
+    guard let url = URL(string: urlString) else {
+      return
+    }
+    
+    let vc = SFSafariViewController(url: url)
+    present(vc, animated: true)
+  }
+
+  func didTapEditProfile() {
+    let vc = EditProfileViewController()
+    vc.title = "Edit Profile"
+    let navVC = UINavigationController(rootViewController: vc)
+    present(navVC, animated: true)
+  }
+  
+  func didTapInviteFriends() {
+    // Show share sheet to invite friends
+  }
+  
+  func didTapSaveOriginalPosts() {
+    
+  }
 }
 
 // MARK: - UITableViewDelegate & UITableViewDataSource
@@ -99,6 +159,7 @@ extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
     cell.textLabel?.text = data[indexPath.section][indexPath.row].title
+    cell.accessoryType = .disclosureIndicator
     return cell
   }
   
